3afee8a06f1b295afa33c37100d8d9c4f59e63c4
Vacxe <aksenov.kostya@gmail.com>
2017-05-25 15:15:58 +0300

    MainActivity: Changing class declaration to interface

--- sample/src/main/java/com/malinskiy/sheldon2/sample/MainActivity.java
+++ sample/src/main/java/com/malinskiy/sheldon2/sample/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Temp_Preferences preferences = new Temp_Preferences(new SharedPreferencesGatewayBuilder(this));
+        MainPreferences preferences = new Temp_Preferences(new SharedPreferencesGatewayBuilder(this));
 
         Log.d(MainActivity.class.getSimpleName(), "String value: " + preferences.getPolicyName().blockingFirst());
         Log.d(MainActivity.class.getSimpleName(), "Enum value: " + preferences.getEnum().blockingFirst());
